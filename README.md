6334dba90b1ce86c3b1c82821319d23d6540e303
Raisa Dorzback <raisa.dorzback@gmail.com>
2024-12-17 13:29:42 -0500

    working 100 api

---
+++ README.md
@@ -0,0 +1,15 @@
+# The 100 Names API
+This API provides birth names, clan affiliations, and nicknames of characters from the 100!
+
+## How To Use
+***Search character by name***
+Make sure to use the character's first name only.
+
+Endpoint: /api/<character-first-name>
+
+***List all characters' data***
+Endpoint: /api
+
+## Optimizations
+- Add the rest of the characters
+- Add images of each character
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+const characters = {
+    'clarke': {
+        'birthName': 'clarke griffin',
+        'clanAffiliation': ['Skaikru', 'Wonkru', 'Finalkru'],
+        'nicknames': [
+            'Wanheda', 
+            'Princess', 
+            'Klark kom Skaikru', 
+            'Fleimkepa',
+            'Mountain Slayer'
+        ]
+    },
+    'octavia': {
+        'birthName': 'Octavia Blake',
+        'clanAffiliation': ['Skaikru', 'Trikru', 'Wonkru', 'Finalkru'],
+        'nicknames': [
+            'Blodreina',
+            'Skairipa',
+            'Osleya',
+            'Girl Under the Floor',
+            'Oktevia kom Skaikru',
+            'Oktevia kom Trikru'
+        ]
+    },
+    'raven': {
+        'birthName': 'Raven Reyes',
+        'clanAffiliation': ['Skaikru', 'Spacekru', 'Wonkru', 'Finalkru'],
+        'nicknames': ['Reyes']
+    },
+    'john' : {
+        'birthName': 'John Murphy',
+        'clanAffiliation': ['Skaikru', 'Spacekru', 'Finalkru'],
+        'nicknames': ['Murphy', 'Fleimkepa', 'Cockroach']
+    }
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get('/api', (req, res) => {
+    res.json(characters);
+});
+
+app.get('/api/:name', (req, res) => {
+    const characterName = req.params.name.toLowerCase();
+    if(characters[characterName]) {
+        res.json([characters[characterName]]);
+    } else {
+        res.status(404).send({ message: "We couldn't find that character in our system." }); 
+    };
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}, better go catch it!`);
+});
